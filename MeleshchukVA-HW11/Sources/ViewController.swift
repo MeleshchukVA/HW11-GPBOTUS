5a0490fe325b62359ff89b48c9ce14bdb2b8c9ac
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-10-19 02:53:25 +0300

    Setup project without storyboards

---
+++ MeleshchukVA-HW11/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MeleshchukVA-HW11
+//
+//  Created by Владимир Мелещук on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
